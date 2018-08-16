9b922578a0939429590447328407d610589fa581
zhushixia <zhushixia@qq.com>
2018-08-16 21:22:53 +0800

    集成数据库迁移的命令

--- manager.py
+++ manager.py
@@ -43,7 +43,7 @@ manager = ("db", MigrateCommand)
 @app.route("/")
 def index():
     session["name"] = "itheima"
-    return "index112411"
+    return "index11241"
 
 if __name__ == "__main__":
     manager.run()
