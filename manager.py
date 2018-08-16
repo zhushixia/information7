e8f534118880d7510d3ce6daf5f8109cfad92a22
zhushixia <zhushixia@qq.com>
2018-08-16 21:18:38 +0800

    集成数据库迁移的命令

--- manager.py
+++ manager.py
@@ -1,13 +1,13 @@
 import redis
-from flask import Flask,session
+from flask import Flask, session
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.wtf import CSRFProtect
 from redis import StrictRedis
-from flask_session import Session#可以指定session保存到位置
+from flask_session import Session  # 可以指定session保存到位置
 from flask_script import Manager
-from flask_migrate import Migrate,MigrateCommand
+from flask_migrate import Migrate, MigrateCommand
 
-class Config(object):#项目的配置
+class Config(object):  # 项目的配置
     DEBUG = True
     SECRET_KEY = "LzUA6O2R2R/mNSD6cbQ5Fzqkq+5VUa3B9/42IG9uoVSzRFbqf2Q6ZGUSVNa4JDstGSADO7hEsQhwk6papcOs4g=="
     #为数据库添加配置
@@ -24,17 +24,17 @@ class Config(object):#项目的配置
     SESSION_PERMANENT = False
 
 app = Flask(__name__)
-#加载配置
+# 加载配置
 app.config.from_object(Config)
-#初始化数据库
+# 初始化数据库
 db = SQLAlchemy(app)
-#初始化redis存储对象
+# 初始化redis存储对象
 redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
-#开启当前项目csrf保护,只做服务验证工作
+# 开启当前项目csrf保护,只做服务验证工作
 CSRFProtect(app)
 Session(app)
 manager = Manager(app)
-#将app与db关联
+# 将app与db关联
 Migrate(app, db)
 #将迁移命令添加到manager中
 manager = ("db", MigrateCommand)
